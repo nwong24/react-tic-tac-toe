372bbe3549f73421dd479e23915466370cab701c
Nathan Wong <nathanwong1776@gmail.com>
2026-03-05 21:13:02 -1000

    complete challenge 1

--- src/App.js
+++ src/App.js
@@ -53,22 +53,13 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
+  const re = /^(?:(?:...){0,2}([OX])\1\1|.{0,2}([OX])..\2..\2|([OX])...\3...\3|..([OX]).\4.\4)/g;
+  const myString = squares.map(s => s === null ? '-' : s).join('');
+  const match = re.exec(myString);
+
+  if (match)
+    return match[1] || match[2] || match[3] || match[4];
+  
   return null;
 }
 
